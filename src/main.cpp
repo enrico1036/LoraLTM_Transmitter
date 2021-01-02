23519e017f000abb044c9b5da80e9fa5d0fd0801
Enrico Gambini <enrico1036@gmail.com>
2021-01-02 02:07:17 +0100

    Long range configuration

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,13 @@
 #include <LoRa.h>
 #include "LTM.hpp"
 
-#define TRANSMIT_PERIOD 1000
+#define TRANSMIT_PERIOD 5000
+
 #define LORA_FREQ 868E6
 #define LORA_TX_POWER 20
-#define LORA_BANDWIDTH 31.25E3
-#define LORA_CODING_RATE 6
+#define LORA_BANDWIDTH 62.5E3
+#define LORA_CODING_RATE 5
+#define LORA_SPREADING_FACTOR 12
 
 HardwareSerial gpsSerial(2);
 TinyGPSPlus gps;
@@ -19,6 +21,9 @@ void setup() {
   gpsSerial.begin(9600);
   Serial.begin(115200);
 
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
   LoRa.setPins(LORA_CS, LORA_RST, LORA_IRQ);
 	LoRa.begin(LORA_FREQ);
 	LoRa.sleep();
@@ -26,7 +31,11 @@ void setup() {
 	LoRa.setTxPower(LORA_TX_POWER, PA_OUTPUT_PA_BOOST_PIN);
 	LoRa.setSignalBandwidth(LORA_BANDWIDTH);
 	LoRa.setCodingRate4(LORA_CODING_RATE);
+  LoRa.setSpreadingFactor(LORA_SPREADING_FACTOR);
 	LoRa.idle();
+
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
@@ -46,8 +55,10 @@ void loop() {
           Serial.println(ltm_gps_frame.toString());
         }
       }
+      digitalWrite(LED_BUILTIN, HIGH);
       LoRa.beginPacket();
       LoRa.write(ltm_gps_frame.toCharBuff(), ltm_gps_frame.getSize());
       LoRa.endPacket();
+      digitalWrite(LED_BUILTIN, LOW);
   }
 }
